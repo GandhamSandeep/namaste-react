91aeb352e7e0a58bc142e2349f78d9643dfe16b5
gandham.sandeep <gandham.sandeep@dhani.com>
2025-07-19 12:35:22 +0530

    Episode-03: Laying the foundation

--- App.js
+++ App.js
@@ -1,47 +1,18 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// First Hello world program in React
-// const heading = React.createElement('h1', {id: 'heading', xyz: 'xyz'}, 'Hello World.!');
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
-// console.log(heading);
 
-
-
-//Nested Elemetns
-const parent = React.createElement(
-  'div',
-  {id: 'parent'}, 
-  React.createElement(
-    'div',
-    {id: 'child'},[
-      React.createElement('h1',{key: '1'},'This is a React Project'),
-      React.createElement('h2',{key: '2'},'second h1 tag')
-     ]),
-  React.createElement(
-    'div',
-    {id: 'child2'},[
-      React.createElement('h1',{key: '3'},'first h1 tag'),
-      React.createElement('h2',{key: '4'},'second h1 tag')
-  ])
-
+const Title = ()=> ( 
+  <h1>React using JSX</h1>
 );
-console.log(parent); //Object
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(parent);
-
-// Output : 
-{/* <div id="root">
-  <div id="parent">
-    <div id="child">
-      <h1>first h1 tag</h1>
-      <h2>second h1 tag</h2>
-    </div>
-    <div id="child">
-      <h1>first h1 tag</h1>
-      <h2>second h1 tag</h2>
-    </div>
+const HeadingComponent = ()=>(
+  <div id="contianer">
+    <Title />
+    <h1 className="heading">Sandeep is here..!</h1>
   </div>
-</div> */}
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HeadingComponent/>);
